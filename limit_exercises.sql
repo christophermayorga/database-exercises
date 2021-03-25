d4ed4b0aa4288c47d92e472fa608ddfd1fb93512
Christopher Mayorga <christopher.mayorga2@gmail.com>
2021-03-25 16:07:54 -0500

    Add exercises using LIMIT

---
+++ limit_exercises.sql
@@ -0,0 +1,32 @@
+USE employees;
+
+SELECT DISTINCT title
+FROM titles;
+
+-- 2. Zykh
+-- Zyda
+-- Zwicker
+-- Zweizig
+-- Zumaque
+-- Zultner
+-- Zucker
+-- Zuberek
+-- Zschoche
+-- Zongker
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+-- 3.Alselm, Cappello, Utz Mandell, Bouchung Schreiter, Baocai Kushner, Petter Stroutstrup
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%' AND birth_date LIKE '%12-25'
+ORDER BY hire_date
+LIMIT 5;
+
+-- 4. Zvonko Nyanchama, Florian Syrotiuk, Basil Tramer, Yinghua Dredge, Hidefumi Caine
+-- the relationship is that they are friends 
+SELECT *
+FROM employees
+LIMIT 5 OFFSET 46;
